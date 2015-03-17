67bd9edec4d7864229a7fa64a90fa81d2e4e050b
sharthk2u <sharathk2u@gmail.com>
2015-03-17 16:42:02 +0530

    version 6

--- src/TskSch/validate/validator.go
+++ src/TskSch/validate/validator.go
@@ -1,15 +1,3 @@
-
-//type Result struct {
-//	Task_id   string //command ID
-//	Executed  bool   //Executed staus
-//	TOE       string //Time Of Execution
-//	TTE       string //Time Taken to Execute
-//	Pid       int    // process id of client
-//	Exec_Stat bool   //Execution Status
-//	output    string
-//	err       string
-//}
-
 package main
 
 import (
@@ -34,49 +22,53 @@ type taskp struct {
 }
 
 func main() {
-	fmt.Println("OOKK")
-	var ids []string
-	res1 := []task{}
-	res2 := []taskp{}
-	session := resultDB.ResultdbInit()
-	Conn := msgQ.RedisInit()
-	Ids, Err := redis.Values(Conn.Do("LRANGE", "task", "0", "-1"))
-	if Err != nil {
-		fmt.Println(Err)
-	}
-	for _, val := range Ids {
-		ids = append(ids, string(val.([]byte)))
-	}
-	session.SetMode(mgo.Monotonic, true)
-	col := session.DB("TskSch").C("Result")
-	Err = col.Find(bson.M{"executed": false, "exec_stat": false}).Select(bson.M{"task_id":1}).All(&res1)
-	if Err != nil {
-		fmt.Println(Err)
-	}
-	Err = col.Find(bson.M{"executed": false, "exec_stat": false ,"pid":bson.M{"$gt":0}}).Select(bson.M{"task_id":1,"pid":1}).All(&res2)
-	if Err != nil {
-		fmt.Println(Err)
-	}
-	for _, val := range res1{
-		flag := In(val.Task_id , ids )
-		if flag != true {
-			x, err := Conn.Do("RPUSH", "task", val.Task_id)
-			if err != nil {
-				fmt.Println(x,err)
-			}
+	for{
+		var ids []string
+		res1 := []task{}
+		res2 := []taskp{}
+		session := resultDB.ResultdbInit()
+		Conn := msgQ.RedisInit()
+		Ids, Err := redis.Values(Conn.Do("LRANGE", "task", "0", "-1"))
+		if Err != nil {
+			fmt.Println(Err)
 		}
-	}
-	for _ , val := range res2{
-		flag := Isalive(val.Pid)
-		if flag != true {
-			flag1 := In(val.Task_id , ids )
-			if flag1 != true {
+		for _, val := range Ids {
+			ids = append(ids, string(val.([]byte)))
+		}
+		session.SetMode(mgo.Monotonic, true)
+		col := session.DB("TskSch").C("Result")
+		Err = col.Find(bson.M{"executed": false, "exec_stat": false}).Select(bson.M{"task_id":1}).All(&res1)
+		if Err != nil {
+			fmt.Println(Err)
+		}
+		Err = col.Find(bson.M{"executed": false, "exec_stat": false ,"pid":bson.M{"$gt":0}}).Select(bson.M{"task_id":1,"pid":1}).All(&res2)
+		if Err != nil {
+			fmt.Println(Err)
+		}
+		for _, val := range res1{
+			flag := In(val.Task_id , ids )
+			if flag != true {
 				x, err := Conn.Do("RPUSH", "task", val.Task_id)
 				if err != nil {
 					fmt.Println(x,err)
 				}
+				fmt.Println("PUSHED" + val.Task_id + "TASK TO msgQ" )
 			}
 		}
+		for _ , val := range res2{
+			flag := Isalive(val.Pid)
+			if flag != true {
+				flag1 := In(val.Task_id , ids )
+				if flag1 != true {
+					x, err := Conn.Do("RPUSH", "task", val.Task_id)
+					if err != nil {
+						fmt.Println(x,err)
+					}
+					fmt.Println("PUSHED" + val.Task_id + "TASK TO msgQ" )
+				}
+			}
+		}
+	time.Sleep(time.Second * 100)
 	}
 }
 func Isalive( pid int ) bool {
